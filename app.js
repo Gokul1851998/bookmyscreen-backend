59505fc1a44c61bf141580999dd1259e81b5ecbe
gokul <gokulmjp@gmail.com>
2023-05-09 13:01:28 +0530

    next14

--- app.js
+++ app.js
@@ -20,20 +20,16 @@ app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({limit:"50mb",extended:true,parameterLimit:50000}))
 app.use(logger('dev'))
 connection()
-app.use(cors(
-//     {
-//     origin:'https://bookmyscreen.netlify.app/',
-//     methods:['GET','POST','PUT','DELETE','PATCH'],
-//     credentials:true,
-//     allowedHeaders:[
-//         'Content-type',
-//         'Access',
-//         'Authorization'
-//     ]
-// }
-)
-
-) 
+app.use(cors({
+    origin:'*',
+    methods:['GET','POST','PUT','DELETE','PATCH'],
+    credentials:true,
+    allowedHeaders:[
+        'Content-type',
+        'Access',
+        'Authorization'
+    ]
+})) 
 
 
  
@@ -48,7 +44,7 @@ const server = app.listen(3000,()=>{
 
 const io = new Server(server, {
     cors: {
-      origin: "https://bookmyscreen.netlify.app/",
+      origin: "*",
       credentials: true,
     },
   });
