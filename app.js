ee47847af9c61b6941d54b63c07e223e52d9a3a1
gokul <gokulmjp@gmail.com>
2023-05-11 17:55:55 +0530

    startnow

--- app.js
+++ app.js
@@ -11,16 +11,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import { Server } from 'socket.io'
 dotenv.config() 
-const app = express();
-
-const corsOptions = {
-    origin: ["https://bookmyscreen.netlify.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: "Content-Type,Authorization",
-    optionsSuccessStatus: 200,
-  };
-  
-app.use(cors(corsOptions));
+const app = express()
  
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}))
@@ -29,7 +20,9 @@ app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({limit:"50mb",extended:true,parameterLimit:50000}))
 app.use(logger('dev'))
 connection()
-
+app.use(
+    cors()
+) 
 app.use('/admin',adminRouter)
 app.use('/',userRouter)
 app.use('/owner',ownerRouter)
