1e02e7eb9b20f1f64bebfa2f3f28c6c144519324
gokul <gokulmjp@gmail.com>
2023-05-11 21:20:44 +0530

    cors changed

--- app.js
+++ app.js
@@ -20,9 +20,17 @@ app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({limit:"50mb",extended:true,parameterLimit:50000}))
 app.use(logger('dev'))
 connection()
-app.use(
-    cors()
-) 
+app.use(cors({ 
+    origin:'https://bookmyscreen.netlify.app',
+    // origin:'http://localhost:8000',
+    method:['POST', 'GET', 'PUT', 'DELETE','PATCH'],
+    credentials: true,
+    allowedHeaders: [
+        'Content-Type', 
+        'Access',
+        'Authorization'
+    ]
+}))   
 app.use('/admin',adminRouter)
 app.use('/',userRouter)
 app.use('/owner',ownerRouter)
