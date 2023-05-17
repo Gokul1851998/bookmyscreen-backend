262bd1b24221df5c3d8ec09161adab7c7cd33313
gokul <gokulmjp@gmail.com>
2023-05-17 18:38:28 +0530

    coo

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -487,66 +487,71 @@ export const getBalance = async(req,res)=>{
             if(userfind.wallet >= total){
                 const selectedSeatIds = selectedSeats.map(seat => seat.id);
 
-                const result = await showModel.aggregate([
-                  {
-                    $match: {
-                      _id: _id
-                    }
-                  },
-                  {
-                    $unwind: "$dates"
-                  },
-                  {
-                    $match: {
-                      "dates.date": new Date(newdate),
-                      "dates.seats.id": { $in: selectedSeatIds }
-                    }
-                  },
-                  {
-                    $set: {
-                      "dates.seats": {
-                        $map: {
-                          input: "$dates.seats",
-                          as: "seat",
-                          in: {
-                            $cond: [
-                              { $in: ["$$seat.id", selectedSeatIds] },
-                              { $mergeObjects: ["$$seat", { seatStatus: "sold" }] },
-                              "$$seat"
-                            ]
-                          }
-                        }
-                      }
-                    }
-                  },
-                  {
-                    $group: {
-                      _id: "$_id",
-                      screenId: { $first: "$screenId" },
-                      ownerId: { $first: "$ownerId" },
-                      ownerName: { $first: "$ownerName" },
-                      location: { $first: "$location" },
-                      movieName: { $first: "$movieName" },
-                      showTime: { $first: "$showTime" },
-                      startDate: { $first: "$startDate" },
-                      endDate: { $first: "$endDate" },
-                      price: { $first: "$price" },
-                      screen: { $first: "$screen" },
-                      dates: { $push: "$dates" },
-                      createdAt: { $first: "$createdAt" },
-                      updatedAt: { $first: "$updatedAt" },
-                      __v: { $first: "$__v" }
-                    }
-                  }
-                ]);
-                
-                if (result.length > 0) {
-                  const updatedShow = result[0];
-                  await showModel.findByIdAndUpdate(_id, updatedShow);
-                  console.log("Seat status updated successfully.");
-                } else {
-                  console.log("No matching show found.");
-                }
+const result = await showModel.aggregate([
+  {
+    $match: {
+      _id: ObjectId(_id)
+    }
+  },
+  {
+    $unwind: "$dates"
+  },
+  {
+    $match: {
+      "dates.date": new Date(newdate),
+      "dates.seats.id": { $in: selectedSeatIds }
+    }
+  },
+  {
+    $set: {
+      "dates.seats": {
+        $map: {
+          input: "$dates.seats",
+          as: "seat",
+          in: {
+            $cond: [
+              { $in: ["$$seat.id", selectedSeatIds] },
+              {
+                $mergeObjects: [
+                  "$$seat",
+                  { seatStatus: { $cond: [{ $eq: ["$$seat.id", "A-1"] }, "sold", "$$seat.seatStatus"] } }
+                ]
+              },
+              "$$seat"
+            ]
+          }
+        }
+      }
+    }
+  },
+  {
+    $group: {
+      _id: "$_id",
+      screenId: { $first: "$screenId" },
+      ownerId: { $first: "$ownerId" },
+      ownerName: { $first: "$ownerName" },
+      location: { $first: "$location" },
+      movieName: { $first: "$movieName" },
+      showTime: { $first: "$showTime" },
+      startDate: { $first: "$startDate" },
+      endDate: { $first: "$endDate" },
+      price: { $first: "$price" },
+      screen: { $first: "$screen" },
+      dates: { $push: "$dates" },
+      createdAt: { $first: "$createdAt" },
+      updatedAt: { $first: "$updatedAt" },
+      __v: { $first: "$__v" }
+    }
+  }
+]);
+
+if (result.length > 0) {
+  const updatedShow = result[0];
+  await showModel.findByIdAndUpdate(_id, updatedShow);
+  console.log("Seat status updated successfully.");
+} else {
+  console.log("No matching show found.");
+}
                   console.log('hree');
                   const newOrder = new orderModel({
                     userId,
