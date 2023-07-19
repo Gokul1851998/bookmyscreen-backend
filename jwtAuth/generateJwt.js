36004d003c3a3001a2b42a1d429f8f5d60c4cbac
Gokul1851998 <114995992+Gokul1851998@users.noreply.github.com>
2023-07-19 09:42:37 +0530

    authication updation

--- jwtAuth/generateJwt.js
+++ jwtAuth/generateJwt.js
@@ -23,6 +23,7 @@ export const generateToken = (details)=>{
 export const userAuthication =(req,res,next)=>{
         try {   
             const authHeader =  req.headers.authorization
+            console.log(authHeader);
             const token = authHeader.split(' ')[1];
             const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
             res.locals.Id = decoded.userId
